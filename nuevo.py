8745adcbc782bf17b0fbf8531529430934b4aa00
javierlindez-collab <javier.lindez@gmail.com>
2026-02-22 21:19:02 +0100

    modificado prueba.py

--- nuevo.py
+++ nuevo.py
@@ -6,5 +6,5 @@ Pruebas trabajo en local con GitHub
 ### Hola aquí va el código python 
 
 print("Esto es una prueba de trabajo con un fichero python en local y en GitHub")
-
+print("Yo lo modifico en mi ordenador en mi Repositorio y luego lo mando al GitHub")
 
