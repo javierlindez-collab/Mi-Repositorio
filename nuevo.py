ec5fa9efa3756af1c95f41225a5ebc2e1857dc82
javierlindez-collab <javier.lindez@gmail.com>
2026-02-24 17:35:18 +0100

    modifico nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -7,4 +7,5 @@ Pruebas trabajo en local con GitHub
 
 print("Esto es una prueba de trabajo con un fichero python en local y en GitHub")
 print("Yo lo modifico en mi ordenador en mi Repositorio y luego lo mando al GitHub")
+print("Ahora hago una modificación a ver si aparece en GitHub")
 
